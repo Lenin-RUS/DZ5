620ca74265c92a5509f77ed1d02e42e20d047db6
Vivo <S.Lanin@gmail.com>
2019-11-04 11:01:37 +0300

    From_Vivo

---
+++ test_python.py
@@ -0,0 +1,10 @@
+import math
+print(math.pi, math.sqrt(4), math.pow(2,2), math.hypot(3,4))
+a=(6,4,7,9,3)
+print(sorted(a))
+print(list(map(lambda x: x**x, a)))
+print(list(filter(lambda x: x==4, a)))
+
+def test_pi:
+    assert
+
