1c54a5873b20e10c4ebcba675a95ecff47a7cdb1
Lenin <S.Lanin@gmail.com>
2019-11-10 20:03:21 +0300

    DZ7_all_good_2

--- account_usage.py
+++ account_usage.py
@@ -1,15 +1,40 @@
-account = 0.0
-purchases = {}
+acc_data_file = 'acc_data.txt'
+orders_data_file='order_data.txt'
+import os
+
+def get_acc_data():
+    if not acc_data_file in os.listdir():
+        f=open(acc_data_file, 'x')
+        f.write('0')
+        f.close
+    with open(acc_data_file, 'r') as f:
+        account=f.read()
+    return float(account)
+
+def ch_acc_data(acc_sum):
+    with open(acc_data_file, 'w') as f:
+        f.write(str(acc_sum))
+
+def add_order(spend, buy_target):
+    if not orders_data_file in os.listdir():
+        f=open(orders_data_file, 'x', encoding='utf8')
+        f.close
+    with open(orders_data_file, 'a', encoding='utf8') as f:
+            f.write(f'{buy_target},{spend} \n')
+
+def all_orders():
+    with open(orders_data_file, 'r', encoding='utf8') as f:
+        for line in f:
+            z=line.split(',')
+            print(f'Покупка: {z[0]}, на сумму: {z[1]}', end='')
+
+
+
 
 def play_account_use():
 
-    from IPython.display import clear_output
-    clear_output()
-
     def action(num_of_action):
-        global account
-        global purchases
-
+        account =  get_acc_data()  # Прочитали начальное состояние счета
         if num_of_action == 1:
             acc_add = float(input(f'Введите сумму пополнения счета: '))
             account +=  acc_add
@@ -21,11 +46,13 @@ def play_account_use():
             else:
                 buy_target = input(f'Введите название товара: ')
                 account -= spend
-                purchases[buy_target] = purchases.setdefault(buy_target, 0) + spend
+                add_order(spend, buy_target)
 
         if num_of_action == 3:
-            for k, v in purchases.items():
-                print(f'на покупку {k} потратили {v} миллиародов долларов Зимбабве.')
+            all_orders()
+
+
+        ch_acc_data(account)  # Занесли состояние счет после операции
 
 
     while True:
